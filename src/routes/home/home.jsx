83b35a56c49fb41ff7aace92d0469e0bdf2bd6f0
gabsonia_ANAS <anas.reish.77@gmail.com>
2023-12-18 02:13:34 +0300

    View - new mark list

--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,22 +1,23 @@
 import React from "react";
-import { theme } from "antd";
+import {theme} from "antd";
+
 const Home = () => {
-  const {
-    token: { colorBgContainer, borderRadiusLG },
-  } = theme.useToken();
-  return (
-    <div
-      style={{
-        padding: 24,
-        minHeight: 380,
-        background: colorBgContainer,
-        borderRadius: borderRadiusLG,
-        marginTop: "15px",
-      }}
-    >
-      Home
-    </div>
-  );
+    const {
+        token: {colorBgContainer, borderRadiusLG},
+    } = theme.useToken();
+    return (
+        <div
+            style={{
+                padding: 24,
+                minHeight: 380,
+                background: colorBgContainer,
+                borderRadius: borderRadiusLG,
+                marginTop: "15px",
+            }}
+        >
+            Home
+        </div>
+    );
 };
 
 export default Home;
