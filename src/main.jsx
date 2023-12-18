24cad6cf04854ed0d202ffa32c2219e2e69ae4f2
gabsonia_ANAS <anas.reish.77@gmail.com>
2023-12-18 16:51:07 +0300

    View - Marks list

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,30 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {ConfigProvider} from "antd";
 import "./index.css";
 import Root from "./routes/root.jsx";
-import Home from "./routes/home/home.jsx";
 import ErrorPage from "./error-page.jsx";
 import ViewNewMarks from "./routes/marks/view-newMarks.jsx";
+import ViewMarks from "./routes/marks/view-Marks.jsx";
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <Root/>,
         errorElement: <ErrorPage/>,
-        children: [{
-            path: "marks",
-            element: <ViewNewMarks/>
-        }],
+        children: [
+            {
+                path: "marks",
+                children: [
+                    {
+                        path: 'new',
+                        element: <ViewNewMarks/>
+                    },
+                    {
+                        path: "view",
+                        element: <ViewMarks/>
+                    },
+                ]
+            },
+        ],
     },
 ]);
 
