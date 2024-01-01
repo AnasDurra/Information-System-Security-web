15cf5c4e1697b06e2b31c12cb853184b555fbe81
gabsonia_ANAS <anas.reish.77@gmail.com>
2024-01-01 17:19:59 +0300

    fix- hash salt

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,14 @@ function App() {
     authSocket.on('registrationResult', onRegistrationResultEvent);
     authSocket.on('loginResult', onLoginResultEvent);
     authSocket.on('completeInfoResult', onCompleteInfoResult);
+    authSocket.on('addMarksResult', onAddMarksResult);
 
     function onLoginResultEvent(msg) {
       console.log('Received message from server:', msg);
       if (msg.status >= 400 && msg.status < 500) console.log('LOGIN REJECTED', msg.error);
       else if (msg.status >= 200 && msg.status < 300) {
         console.log('LOGIN ACCEPTED');
+
         login(msg.data.access_token);
 
         navigate('/', { replace: true });
@@ -39,9 +41,15 @@ function App() {
       console.log('Received message from server:', msg);
     }
 
+    function onAddMarksResult(msg) {
+      console.log('Received message from server:', msg);
+    }
+
     return () => {
       authSocket.off('registrationResult', onRegistrationResultEvent);
       authSocket.off('loginResult', onLoginResultEvent);
+      authSocket.off('completeInfoResult', onLoginResultEvent);
+      authSocket.off('addMarksResult', onLoginResultEvent);
     };
   }, []);
 
