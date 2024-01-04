e83d673eb0570f4ff7fe4db62e86258c0555ceb4
gabsonia_ANAS <anas.reish.77@gmail.com>
2024-01-04 06:16:19 +0300

    merge fixes

--- src/routes/marks/view-marks.jsx
+++ src/routes/marks/view-marks.jsx
@@ -55,7 +55,7 @@ function ViewMarks(props) {
     });
     marksSocket.connect();
 
-    getsocket.connect();
+    getSocket.connect();
 
     requestGetTeacherSubjects({ access_token: authToken });
 
